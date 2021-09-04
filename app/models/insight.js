b316c5f2f6cd18f12d697c1613640ea43a3cd1e3
Christian Wibisono <christian.wibisono7@gmail.com>
2021-09-05 00:05:34 +0800

    Return insight data

--- app/models/insight.js
+++ app/models/insight.js
@@ -1,4 +1,5 @@
 const {getClient} = require("../database/datastax")
+const videoDetailModel = require("../models/video_detail");
 
 async function getInsight(videoId) {
   let query = `SELECT * from touree.video_event_tab where video_id = '${videoId}'`
@@ -18,6 +19,14 @@ async function getInsight(videoId) {
     }
     countMap[row['current_video_id']] = 1
   })
+
+  const vidDetails = await videoDetailModel.getByVideoId(videoId)
+
+  for (let i = 0; i < vidDetails.length; i++) {
+    if (vidDetails[i]['video_id'].toString() in countMap) {
+      vidDetails[i].count = countMap[vidDetails[i]['video_id'].toString()]
+    }
+  }
 
   return rs
 }
